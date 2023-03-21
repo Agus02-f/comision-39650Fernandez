a182186f9b218813da4be4218794a7c0286d0616
Agus02-f <fernandezagustin276@gmail.com>
2023-03-21 20:54:10 -0300

    first commit

---
+++ src/Login.jsx
@@ -0,0 +1,13 @@
+export const Login = () => {
+    Let nombre = "pepito";
+
+    const saludar = () => {
+        console.log("Hola como estan?")
+    }
+    return (<div>
+        <h1>Hola desde el component login</h1>
+        <h3>mi nombre es {nombre} </h3>
+        <button onClick={ }>{saludar}Saludar</button>
+    </div>
+    )
+}
